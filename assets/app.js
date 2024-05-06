2f3a4c9c6ce3338469899baeda06112d993456d2
DylanQuenon <dylan.quenon.04@gmail.com>
2024-05-06 12:59:44 +0200

    installations

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,10 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
+import './styles/bootstrap.min.css';
+import './styles/pages/home.scss'
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('home.html.twig', [
+            
+        ]);
+    }
+}
