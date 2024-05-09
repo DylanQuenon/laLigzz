ee3e65078bb344be366851b43548a3766c5dc1c4
DylanQuenon <dylan.quenon.04@gmail.com>
2024-05-09 15:12:42 +0200

    addSliderTeam

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TeamRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(TeamRepository $team): Response
     {
+        $teams = $team->findAll();
         return $this->render('home.html.twig', [
+            'teams' => $teams
             
         ]);
     }
