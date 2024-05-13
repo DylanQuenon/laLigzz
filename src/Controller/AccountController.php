df7ea9c07c388226089fa7a7f14ed804513b925c
DylanQuenon <dylan.quenon.04@gmail.com>
2024-05-13 17:19:33 +0200

    connexion sans scss + showPassword

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
+
+class AccountController extends AbstractController
+{
+    #[Route('/login', name: 'account_login')]
+    public function index(AuthenticationUtils $utils): Response
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        $loginError = null;
+
+        
+        if($error instanceof TooManyLoginAttemptsAuthenticationException)
+        {
+            // l'ereur est due à la limitation de tentative de connexion
+            $loginError = "Trop de tentatives de connexion. Réessayez plus tard";
+        }
+        return $this->render('account/index.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username,
+            'loginError' => $loginError
+        ]);
+    }
+     /**
+     * Permet de se déconnecter
+     *
+     * @return void
+     */
+    #[Route("/logout", name: "account_logout")]
+    public function logout(): void
+    {
+
+    }
+}
