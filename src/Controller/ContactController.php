ee7c1e69b4360a8033b760a89bec8a849dac1b90
DylanQuenon <dylan.quenon.04@gmail.com>
2024-07-02 11:04:22 +0200

    test mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,34 +4,61 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Dotenv\Dotenv;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'contact')]
-    public function index(Request $request,EntityManagerInterface $manager): Response
+    private $contactEmail;
+
+
+    public function __construct(ParameterBagInterface $params)
+
     {
-        $contact= new Contact();
-        $form = $this->createForm(ContactType::class, $contact);     
+
+        $this->contactEmail = $params->get('contact_email');
+
+    }
+    
+
+    #[Route('/contact', name: 'contact')]
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             $manager->persist($contact);
             $manager->flush();
-            $this->addFlash('success', 'Votre message a bien été envoyé');
+
+            
+            $email = (new Email())
+                ->from('contact@laligzz.dylanquenon.com')  // E-mail de l'expéditeur
+                ->to($this->contactEmail)  // Utilisation de l'e-mail du destinataire récupéré depuis .env
+                ->subject($contact->getSubject())
+                ->html($this->renderView('mail/mail.html.twig', [
+                    'contact' => $contact,
+                ]));
+
+            // Envoi de l'e-mail
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a bien été envoyé et nous vous répondrons sous peu.');
             return $this->redirectToRoute('contact');
         }
+
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView()
         ]);
     }
-    
-
-
-    
 }
